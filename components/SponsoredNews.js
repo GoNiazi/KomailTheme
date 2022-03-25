f8db0e947ec462940f412f1a6928514b6775fc1c
GoNiazi <komailniazi@gmail.com>
2022-03-25 20:17:34 +0500

    componentsgone

--- components/SponsoredNews.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const SponsoredNews = () => {
-  return (
-   
-  );
-};
-
-export default SponsoredNews;
--- components/TrendingStart.js
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-
-// const TrendingStart = () => {
-//   return (
-
-//   );
-// };
-
-// export default TrendingStart;
